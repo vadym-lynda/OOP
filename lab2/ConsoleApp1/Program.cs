15448f1330d8b1357978a66a402ce75cc0eca3c0
vadym-lynda <112697730+vadym-lynda@users.noreply.github.com>
2024-12-22 09:43:15 +0200

    Add files via upload

---
+++ lab2/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Введіть x:");
+            string inputX = Console.ReadLine();
+            Console.WriteLine("Введіть y:");
+            string inputY = Console.ReadLine();
+            Console.WriteLine("Введіть z:");
+            string inputZ = Console.ReadLine();
+
+            int x, y, z;
+
+            if (!int.TryParse(inputX, out x))
+            {
+                Console.WriteLine("Некоректне значення для x.");
+                Console.ReadLine();
+                return;
+            }
+
+            if (!int.TryParse(inputY, out y))
+            {
+                Console.WriteLine("Некоректне значення для y.");
+                Console.ReadLine();
+                return;
+
+            }
+
+            if (!int.TryParse(inputZ, out z))
+            {
+                Console.WriteLine("Некоректне значення для z.");
+                Console.ReadLine();
+                return;
+
+            }
+
+
+            double res1 = 1 + Math.Sin(x + y);
+            double res2 = Math.Abs((2 * x) / (1 + x + Math.Pow(z, 3)));
+            double res = (res1 / res2) * (Math.Pow(x, Math.Abs(y)));
+
+            Console.WriteLine("Результат: " + Math.Round(res, 4));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab2/lab2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            int x, y, z;
+
+            if (!int.TryParse(textBox1.Text, out x))
+            {
+                MessageBox.Show("Будь ласка, введіть правильне значення для X.");
+            }
+            else if (!int.TryParse(textBox2.Text, out y))
+            {
+                MessageBox.Show("Будь ласка, введіть правильне значення для Y.");
+            }
+            else if (!int.TryParse(textBox3.Text, out z))
+            {
+                MessageBox.Show("Будь ласка, введіть правильне значення для Z.");
+            }
+            else
+            {
+                double res1 = 1 + Math.Sin(x+y);
+                double res2 = Math.Abs((2*x)/(1+x+Math.Pow(z,3)));
+                double res = (res1/res2)*(Math.Pow(x,Math.Abs(y)));
+
+                MessageBox.Show("Результат: " + Math.Round(res, 4));
+            }
+
+        }
+
+      
+    }
+}
